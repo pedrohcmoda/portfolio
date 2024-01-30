3c74c3953d396828b1bf1bdf94b3fe24a82130ca
pedrohcmoda <phcunha47@gmail.com>
2024-01-30 19:14:33 -0300

    teste rotas

--- constants/index.ts
+++ constants/index.ts
@@ -112,16 +112,16 @@ export const NavLinks = [
   {
     name: "/minhas-skills",
     icon: RxPerson,
-    link: "/minhas-skills",
+    link: "/minhas-skills/page.tsx",
   },
   {
-    name: "/meus-projetos",
+    name: "./meus-projetos",
     icon: RxDashboard,
-    link: "/meus-projetos",
+    link: "/meus-projetos/page.tsx",
   },
   {
     name: "/me-contate",
     icon: RxClipboard,
-    link: "/me-contate",
+    link: "/me-contate/page.tsx",
   },
 ];
